eb62573ce940288ee1a376b3c77faba1e306fbbd
Md Maqsood Ahamad Khan <122003574+bioinfomak@users.noreply.github.com>
2026-01-15 19:17:25 +0530

    Update shiny_maf.R

--- shiny_maf.R
+++ shiny_maf.R
@@ -1,6 +1,7 @@
 ############################################################
-# ALS Targeted Sequencing – Shiny MAF Explorer
-# Includes Sporadic ALS vs Normal & Familial ALS comparisons
+# ALS Shiny MAF Explorer
+# Targeted + WES | SALS (M/F), FALS, Normal
+# NO COHORT MERGING
 ############################################################
 
 suppressPackageStartupMessages({
@@ -11,48 +12,33 @@ suppressPackageStartupMessages({
 })
 
 # ===============================
-# LOAD MAF FILES (in app dir)
+# LOAD TARGETED MAF FILES
 # ===============================
 
-maf_male <- read.maf(
-  "PDXRL_M_SALS_merged.maf",
-  removeDuplicatedVariants = TRUE,
-  isTCGA = FALSE
-)
-
-maf_female <- read.maf(
-  "PDXRL_F_SALS_merged.maf",
-  removeDuplicatedVariants = TRUE,
-  isTCGA = FALSE
-)
-
-maf_normal <- read.maf(
-  "PDXRL_NORMAL_merged.maf",
-  removeDuplicatedVariants = TRUE,
-  isTCGA = FALSE
-)
-
-maf_FALS <- read.maf(
-  "PDXRL_FALS_merged.maf",
-  removeDuplicatedVariants = TRUE,
-  isTCGA = FALSE
-)
+maf_T_M_SALS <- read.maf("PDXRL_M_SALS_merged.maf", isTCGA = FALSE)
+maf_T_F_SALS <- read.maf("PDXRL_F_SALS_merged.maf", isTCGA = FALSE)
+maf_T_FALS   <- read.maf("PDXRL_FALS_merged.maf",   isTCGA = FALSE)
+maf_NORMAL   <- read.maf("PDXRL_NORMAL_merged.maf", isTCGA = FALSE)
 
 # ===============================
-# Add Sex annotation to Male/Female MAFs
+# LOAD WES MAF FILES
 # ===============================
 
-maf_male@clinical.data$Sex   <- "Male"
-maf_female@clinical.data$Sex <- "Female"
+maf_W_M_SALS <- read.maf("WES_M_SALS_merged.maf", isTCGA = FALSE)
+maf_W_F_SALS <- read.maf("WES_F_SALS_merged.maf", isTCGA = FALSE)
+maf_W_FALS   <- read.maf("WES_FALS_merged.maf",   isTCGA = FALSE)
 
 # ===============================
-# Create merged Sporadic ALS MAF (Male + Female)
+# ADD SEX ANNOTATION
 # ===============================
 
-maf_SALS <- merge_mafs(list(maf_male, maf_female))
+maf_T_M_SALS@clinical.data$Sex <- "Male"
+maf_T_F_SALS@clinical.data$Sex <- "Female"
+maf_W_M_SALS@clinical.data$Sex <- "Male"
+maf_W_F_SALS@clinical.data$Sex <- "Female"
 
 # ===============================
-# ALS gene panel
+# ALS GENE PANEL
 # ===============================
 
 als_genes <- c(
@@ -66,21 +52,22 @@ als_genes <- c(
 
 ui <- fluidPage(
   
-  titlePanel("ALS Targeted Sequencing – MAF Explorer"),
+  titlePanel("ALS MAF Explorer (Targeted + WES | No SALS Merging)"),
   
   sidebarLayout(
-    
     sidebarPanel(
       
       selectInput(
         "cohort",
         "Select Cohort",
         choices = c(
-          "Male ALS"   = "M",
-          "Female ALS" = "F",
-          "Normal"     = "N",
-          "Sporadic ALS (M+F)" = "SALS",
-          "Familial ALS" = "FALS"
+          "Targeted M-SALS" = "T_M_SALS",
+          "Targeted F-SALS" = "T_F_SALS",
+          "Targeted FALS"   = "T_FALS",
+          "Normal"          = "NORMAL",
+          "WES M-SALS"      = "W_M_SALS",
+          "WES F-SALS"      = "W_F_SALS",
+          "WES FALS"        = "W_FALS"
         )
       ),
       
@@ -93,11 +80,19 @@ ui <- fluidPage(
           "ALS Gene Panel",
           "Single Gene",
           "Somatic Interactions",
-          "Male vs Female Oncoplot",
-          "Male vs Female Gene Comparison",
-          "Sporadic ALS vs Normal Comparison",
-          "Familial ALS vs Normal Comparison",
-          "Sporadic ALS vs Familial ALS Comparison",
+          
+          "Targeted M-SALS vs Normal",
+          "Targeted F-SALS vs Normal",
+          "Targeted FALS vs Normal",
+          
+          "WES M-SALS vs Normal",
+          "WES F-SALS vs Normal",
+          "WES FALS vs Normal",
+          
+          "Targeted M-SALS vs WES M-SALS",
+          "Targeted F-SALS vs WES F-SALS",
+          "Targeted FALS vs WES FALS",
+          
           "TMB",
           "Ti/Tv",
           "Data Table"
@@ -110,8 +105,7 @@ ui <- fluidPage(
       ),
       
       hr(),
-      
-      downloadButton("download_plot", "Download Figure"),
+      downloadButton("download_plot", "Download Plot"),
       downloadButton("download_table", "Download Table")
     ),
     
@@ -128,19 +122,19 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
   
-  # ---- Select active MAF by cohort input ----
   maf_active <- reactive({
     switch(
       input$cohort,
-      "M" = maf_male,
-      "F" = maf_female,
-      "N" = maf_normal,
-      "SALS" = maf_SALS,
-      "FALS" = maf_FALS
+      "T_M_SALS" = maf_T_M_SALS,
+      "T_F_SALS" = maf_T_F_SALS,
+      "T_FALS"   = maf_T_FALS,
+      "NORMAL"   = maf_NORMAL,
+      "W_M_SALS" = maf_W_M_SALS,
+      "W_F_SALS" = maf_W_F_SALS,
+      "W_FALS"   = maf_W_FALS
     )
   })
   
-  # ---- Dynamic gene selector for Single Gene view ----
   output$gene_ui <- renderUI({
     selectInput(
       "gene",
@@ -149,291 +143,110 @@ server <- function(input, output, session) {
     )
   })
   
-  # ---- Plotting function for all views ----
   make_plot <- function(view, maf, gene = NULL) {
     
     if (view == "Overview") {
-      
-      plotmafSummary(
-        maf,
-        rmOutlier = TRUE,
-        addStat = "median",
-        dashboard = TRUE
-      )
+      plotmafSummary(maf, dashboard = TRUE)
       
     } else if (view == "Oncoplot") {
-      
-      oncoplot(
-        maf,
-        top = 25,
-        removeNonMutated = TRUE
-      )
+      oncoplot(maf, top = 25)
       
     } else if (view == "ALS Gene Panel") {
-      
-      maf_als <- subsetMaf(maf, genes = als_genes, mafObj = TRUE)
-      
-      if (nrow(maf_als@data) == 0) {
-        plot.new()
-        text(0.5, 0.5, "No ALS genes found", cex = 1.3)
-      } else {
-        oncoplot(
-          maf_als,
-          top = length(als_genes),
-          removeNonMutated = TRUE
-        )
-      }
+      oncoplot(subsetMaf(maf, als_genes, mafObj = TRUE),
+               top = length(als_genes))
       
     } else if (view == "Single Gene") {
-      
-      lollipopPlot(
-        maf,
-        gene = gene,
-        AACol = "HGVSp_Short"
-      )
+      lollipopPlot(maf, gene = gene)
       
     } else if (view == "Somatic Interactions") {
+      somaticInteractions(maf, top = 50)
       
-      somaticInteractions(
-        maf,
-        top = 50,
-        pvalue = c(0.05, 0.01)
-      )
+    } else if (view == "Targeted M-SALS vs Normal") {
+      forestPlot(mafCompare(maf_T_M_SALS, maf_NORMAL,
+                            "Targeted M-SALS", "Normal"))
       
-    } else if (view == "Male vs Female Oncoplot") {
+    } else if (view == "Targeted F-SALS vs Normal") {
+      forestPlot(mafCompare(maf_T_F_SALS, maf_NORMAL,
+                            "Targeted F-SALS", "Normal"))
       
-      maf_MF <- merge_mafs(list(maf_male, maf_female))
-      maf_MF@clinical.data$Sex <- factor(maf_MF@clinical.data$Sex)
+    } else if (view == "Targeted FALS vs Normal") {
+      forestPlot(mafCompare(maf_T_FALS, maf_NORMAL,
+                            "Targeted FALS", "Normal"))
       
-      oncoplot(
-        maf = maf_MF,
-        top = 30,
-        clinicalFeatures = "Sex",
-        sortByAnnotation = TRUE,
-        annotationColor = list(Sex = c(Male = "#377EB8", Female = "#E41A1C")),
-        removeNonMutated = TRUE
-      )
+    } else if (view == "WES M-SALS vs Normal") {
+      forestPlot(mafCompare(maf_W_M_SALS, maf_NORMAL,
+                            "WES M-SALS", "Normal"))
       
-    } else if (view == "Male vs Female Gene Comparison") {
+    } else if (view == "WES F-SALS vs Normal") {
+      forestPlot(mafCompare(maf_W_F_SALS, maf_NORMAL,
+                            "WES F-SALS", "Normal"))
       
-      comp <- mafCompare(
-        m1 = maf_male,
-        m2 = maf_female,
-        m1Name = "Male",
-        m2Name = "Female"
-      )
+    } else if (view == "WES FALS vs Normal") {
+      forestPlot(mafCompare(maf_W_FALS, maf_NORMAL,
+                            "WES FALS", "Normal"))
       
-      forestPlot(comp)
+    } else if (view == "Targeted M-SALS vs WES M-SALS") {
+      forestPlot(mafCompare(maf_T_M_SALS, maf_W_M_SALS,
+                            "Targeted M-SALS", "WES M-SALS"))
       
-    } else if (view == "Sporadic ALS vs Normal Comparison") {
+    } else if (view == "Targeted F-SALS vs WES F-SALS") {
+      forestPlot(mafCompare(maf_T_F_SALS, maf_W_F_SALS,
+                            "Targeted F-SALS", "WES F-SALS"))
       
-      comp <- mafCompare(
-        m1 = maf_SALS,
-        m2 = maf_normal,
-        m1Name = "Sporadic ALS",
-        m2Name = "Normal"
-      )
-      
-      forestPlot(comp)
-      
-    } else if (view == "Familial ALS vs Normal Comparison") {
-      
-      comp <- mafCompare(
-        m1 = maf_FALS,
-        m2 = maf_normal,
-        m1Name = "Familial ALS",
-        m2Name = "Normal"
-      )
-      
-      forestPlot(comp)
-      
-    } else if (view == "Sporadic ALS vs Familial ALS Comparison") {
-      
-      comp <- mafCompare(
-        m1 = maf_SALS,
-        m2 = maf_FALS,
-        m1Name = "Sporadic ALS",
-        m2Name = "Familial ALS"
-      )
-      
-      forestPlot(comp)
+    } else if (view == "Targeted FALS vs WES FALS") {
+      forestPlot(mafCompare(maf_T_FALS, maf_W_FALS,
+                            "Targeted FALS", "WES FALS"))
       
     } else if (view == "TMB") {
-      
-      tmb_res <- tmb(maf)
-      
-      ggplot(tmb_res, aes(x = "", y = total_perMB)) +
-        geom_boxplot(outlier.shape = NA) +
-        geom_jitter(width = 0.1, alpha = 0.6) +
-        ylab("Mutations per Mb") +
+      ggplot(tmb(maf), aes(x = "", y = total_perMB)) +
+        geom_boxplot() +
+        ylab("Mutations / Mb") +
         theme_minimal()
       
     } else if (view == "Ti/Tv") {
-      
       plotTiTv(titv(maf))
-      
     }
   }
   
-  # ---- Render main plot ----
   output$mainPlot <- renderPlot({
-    make_plot(
-      view = input$view,
-      maf  = maf_active(),
-      gene = input$gene
-    )
+    make_plot(input$view, maf_active(), input$gene)
   })
   
-  # ---- Render data table ----
   output$summaryTable <- renderTable({
-    
-    maf <- maf_active()
-    
     if (input$view == "Data Table") {
-      
-      head(maf@data, 50)
-      
-    } else if (input$view == "Overview") {
-      
-      getSampleSummary(maf)
-      
-    } else if (input$view == "Male vs Female Gene Comparison") {
-      
-      comp <- mafCompare(
-        m1 = maf_male,
-        m2 = maf_female,
-        m1Name = "Male",
-        m2Name = "Female"
-      )
-      
-      comp$results[order(comp$results$pval), ][1:25, ]
-      
-    } else if (input$view == "Sporadic ALS vs Normal Comparison") {
-      
-      comp <- mafCompare(
-        m1 = maf_SALS,
-        m2 = maf_normal,
-        m1Name = "Sporadic ALS",
-        m2Name = "Normal"
-      )
-      
-      comp$results[order(comp$results$pval), ][1:25, ]
-      
-    } else if (input$view == "Familial ALS vs Normal Comparison") {
-      
-      comp <- mafCompare(
-        m1 = maf_FALS,
-        m2 = maf_normal,
-        m1Name = "Familial ALS",
-        m2Name = "Normal"
-      )
-      
-      comp$results[order(comp$results$pval), ][1:25, ]
-      
-    } else if (input$view == "Sporadic ALS vs Familial ALS Comparison") {
-      
-      comp <- mafCompare(
-        m1 = maf_SALS,
-        m2 = maf_FALS,
-        m1Name = "Sporadic ALS",
-        m2Name = "Familial ALS"
-      )
-      
-      comp$results[order(comp$results$pval), ][1:25, ]
-      
-    } else {
-      NULL
+      head(maf_active()@data, 50)
     }
   })
   
-  # ---- Download plot handler ----
+  # ===============================
+  # DOWNLOAD PLOT
+  # ===============================
+  
   output$download_plot <- downloadHandler(
-    
     filename = function() {
-      paste0(
-        gsub(" ", "_", input$view),
-        "_",
-        Sys.Date(),
-        ".png"
-      )
+      paste0(gsub(" ", "_", input$view), "_", Sys.Date(), ".png")
     },
-    
     content = function(file) {
       png(file, width = 2400, height = 1800, res = 300)
-      
-      make_plot(
-        view = input$view,
-        maf  = maf_active(),
-        gene = input$gene
-      )
-      
+      make_plot(input$view, maf_active(), input$gene)
       dev.off()
     }
   )
   
-  # ---- Download table handler ----
+  # ===============================
+  # DOWNLOAD TABLE
+  # ===============================
+  
   output$download_table <- downloadHandler(
-    
     filename = function() {
-      paste0(
-        gsub(" ", "_", input$view),
-        "_",
-        Sys.Date(),
-        ".csv"
-      )
+      paste0(gsub(" ", "_", input$view), "_", Sys.Date(), ".csv")
     },
-    
     content = function(file) {
-      
-      maf <- maf_active()
       
       df <- switch(
         input$view,
-        
-        "Data Table" = maf@data,
-        
-        "Overview" = getSampleSummary(maf),
-        
-        "Male vs Female Gene Comparison" = {
-          comp <- mafCompare(
-            m1 = maf_male,
-            m2 = maf_female,
-            m1Name = "Male",
-            m2Name = "Female"
-          )
-          comp$results
-        },
-        
-        "Sporadic ALS vs Normal Comparison" = {
-          comp <- mafCompare(
-            m1 = maf_SALS,
-            m2 = maf_normal,
-            m1Name = "Sporadic ALS",
-            m2Name = "Normal"
-          )
-          comp$results
-        },
-        
-        "Familial ALS vs Normal Comparison" = {
-          comp <- mafCompare(
-            m1 = maf_FALS,
-            m2 = maf_normal,
-            m1Name = "Familial ALS",
-            m2Name = "Normal"
-          )
-          comp$results
-        },
-        
-        "Sporadic ALS vs Familial ALS Comparison" = {
-          comp <- mafCompare(
-            m1 = maf_SALS,
-            m2 = maf_FALS,
-            m1Name = "Sporadic ALS",
-            m2Name = "Familial ALS"
-          )
-          comp$results
-        },
-        
+        "Data Table" = maf_active()@data,
+        "Overview"   = getSampleSummary(maf_active()),
         NULL
       )
       
@@ -446,11 +259,8 @@ server <- function(input, output, session) {
 }
 
 # ===============================
-# Run the Shiny app
+# RUN APP
 # ===============================
 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
 
-
-# ---- Run app ----
-shinyApp(ui = ui, server = server)
