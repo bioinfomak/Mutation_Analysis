edfe72cd3e1f841e242c1a2c3c2611f6c08acaed
Md Maqsood Ahamad Khan <122003574+bioinfomak@users.noreply.github.com>
2026-01-16 14:14:53 +0530

    Update shiny_maf.R

--- shiny_maf.R
+++ shiny_maf.R
@@ -1,7 +1,6 @@
 ############################################################
-# ALS Shiny MAF Explorer
-# Targeted + WES | SALS (M/F), FALS, Normal
-# NO COHORT MERGING
+# ALS Shiny MAF Explorer – FINAL CLEAN VERSION
+# Targeted + WES | SALS vs FALS | With p-values
 ############################################################
 
 suppressPackageStartupMessages({
@@ -12,35 +11,20 @@ suppressPackageStartupMessages({
 })
 
 # ===============================
-# LOAD TARGETED MAF FILES
+# LOAD MAF FILES
 # ===============================
-
 maf_T_M_SALS <- read.maf("PDXRL_M_SALS_merged.maf", isTCGA = FALSE)
 maf_T_F_SALS <- read.maf("PDXRL_F_SALS_merged.maf", isTCGA = FALSE)
 maf_T_FALS   <- read.maf("PDXRL_FALS_merged.maf",   isTCGA = FALSE)
 maf_NORMAL   <- read.maf("PDXRL_NORMAL_merged.maf", isTCGA = FALSE)
-
-# ===============================
-# LOAD WES MAF FILES
-# ===============================
 
 maf_W_M_SALS <- read.maf("WES_M_SALS_merged.maf", isTCGA = FALSE)
 maf_W_F_SALS <- read.maf("WES_F_SALS_merged.maf", isTCGA = FALSE)
 maf_W_FALS   <- read.maf("WES_FALS_merged.maf",   isTCGA = FALSE)
 
 # ===============================
-# ADD SEX ANNOTATION
-# ===============================
-
-maf_T_M_SALS@clinical.data$Sex <- "Male"
-maf_T_F_SALS@clinical.data$Sex <- "Female"
-maf_W_M_SALS@clinical.data$Sex <- "Male"
-maf_W_F_SALS@clinical.data$Sex <- "Female"
-
-# ===============================
 # ALS GENE PANEL
 # ===============================
-
 als_genes <- c(
   "SOD1","C9orf72","TARDBP","FUS","OPTN","TBK1",
   "NEK1","SETX","VCP","UBQLN2","ATXN2","ALS2"
@@ -49,18 +33,16 @@ als_genes <- c(
 # ===============================
 # UI
 # ===============================
-
 ui <- fluidPage(
   
-  titlePanel("ALS MAF Explorer (Targeted + WES | No SALS Merging)"),
+  titlePanel("ALS MAF Explorer"),
   
   sidebarLayout(
     sidebarPanel(
       
       selectInput(
-        "cohort",
-        "Select Cohort",
-        choices = c(
+        "cohort", "Select Cohort",
+        c(
           "Targeted M-SALS" = "T_M_SALS",
           "Targeted F-SALS" = "T_F_SALS",
           "Targeted FALS"   = "T_FALS",
@@ -72,14 +54,16 @@ ui <- fluidPage(
       ),
       
       selectInput(
-        "view",
-        "Select Analysis",
-        choices = c(
+        "view", "Select Analysis",
+        c(
           "Overview",
           "Oncoplot",
           "ALS Gene Panel",
           "Single Gene",
           "Somatic Interactions",
+          
+          "Targeted M-SALS vs FALS",
+          "Targeted F-SALS vs FALS",
           
           "Targeted M-SALS vs Normal",
           "Targeted F-SALS vs Normal",
@@ -89,12 +73,14 @@ ui <- fluidPage(
           "WES F-SALS vs Normal",
           "WES FALS vs Normal",
           
-          "Targeted M-SALS vs WES M-SALS",
-          "Targeted F-SALS vs WES F-SALS",
-          "Targeted FALS vs WES FALS",
+          "Targeted vs WES M-SALS",
+          "Targeted vs WES F-SALS",
+          "Targeted vs WES FALS",
           
           "TMB",
           "Ti/Tv",
+          
+          "Gene Mutation Frequency",
           "Data Table"
         )
       ),
@@ -119,30 +105,30 @@ ui <- fluidPage(
 # ===============================
 # SERVER
 # ===============================
-
 server <- function(input, output, session) {
   
   maf_active <- reactive({
-    switch(
-      input$cohort,
-      "T_M_SALS" = maf_T_M_SALS,
-      "T_F_SALS" = maf_T_F_SALS,
-      "T_FALS"   = maf_T_FALS,
-      "NORMAL"   = maf_NORMAL,
-      "W_M_SALS" = maf_W_M_SALS,
-      "W_F_SALS" = maf_W_F_SALS,
-      "W_FALS"   = maf_W_FALS
+    switch(input$cohort,
+           "T_M_SALS" = maf_T_M_SALS,
+           "T_F_SALS" = maf_T_F_SALS,
+           "T_FALS"   = maf_T_FALS,
+           "NORMAL"   = maf_NORMAL,
+           "W_M_SALS" = maf_W_M_SALS,
+           "W_F_SALS" = maf_W_F_SALS,
+           "W_FALS"   = maf_W_FALS
     )
   })
   
   output$gene_ui <- renderUI({
     selectInput(
-      "gene",
-      "Select Gene",
-      choices = sort(unique(maf_active()@gene.summary$Hugo_Symbol))
+      "gene", "Select Gene",
+      sort(unique(maf_active()@gene.summary$Hugo_Symbol))
     )
   })
   
+  # ===============================
+  # SAFE PLOT FUNCTION
+  # ===============================
   make_plot <- function(view, maf, gene = NULL) {
     
     if (view == "Overview") {
@@ -152,8 +138,13 @@ server <- function(input, output, session) {
       oncoplot(maf, top = 25)
       
     } else if (view == "ALS Gene Panel") {
-      oncoplot(subsetMaf(maf, als_genes, mafObj = TRUE),
-               top = length(als_genes))
+      g <- intersect(als_genes, maf@gene.summary$Hugo_Symbol)
+      if (length(g) == 0) {
+        plot.new()
+        text(0.5, 0.5, "No ALS genes detected")
+        return()
+      }
+      oncoplot(subsetMaf(maf, genes = g), top = length(g))
       
     } else if (view == "Single Gene") {
       lollipopPlot(maf, gene = gene)
@@ -161,50 +152,49 @@ server <- function(input, output, session) {
     } else if (view == "Somatic Interactions") {
       somaticInteractions(maf, top = 50)
       
-    } else if (view == "Targeted M-SALS vs Normal") {
-      forestPlot(mafCompare(maf_T_M_SALS, maf_NORMAL,
-                            "Targeted M-SALS", "Normal"))
+      # ===============================
+      # mafCompare with p-values
+      # ===============================
+    } else if (grepl("vs", view)) {
       
-    } else if (view == "Targeted F-SALS vs Normal") {
-      forestPlot(mafCompare(maf_T_F_SALS, maf_NORMAL,
-                            "Targeted F-SALS", "Normal"))
+      cmp <- switch(view,
+                    "Targeted M-SALS vs FALS" =
+                      mafCompare(maf_T_M_SALS, maf_T_FALS, "T_M_SALS", "T_FALS", minMut = 5),
+                    "Targeted F-SALS vs FALS" =
+                      mafCompare(maf_T_F_SALS, maf_T_FALS, "T_F_SALS", "T_FALS", minMut = 5),
+                    "Targeted M-SALS vs Normal" =
+                      mafCompare(maf_T_M_SALS, maf_NORMAL, "T_M_SALS", "Normal", minMut = 5),
+                    "Targeted F-SALS vs Normal" =
+                      mafCompare(maf_T_F_SALS, maf_NORMAL, "T_F_SALS", "Normal", minMut = 5),
+                    "Targeted FALS vs Normal" =
+                      mafCompare(maf_T_FALS, maf_NORMAL, "T_FALS", "Normal", minMut = 5),
+                    "WES M-SALS vs Normal" =
+                      mafCompare(maf_W_M_SALS, maf_NORMAL, "W_M_SALS", "Normal", minMut = 5),
+                    "WES F-SALS vs Normal" =
+                      mafCompare(maf_W_F_SALS, maf_NORMAL, "W_F_SALS", "Normal", minMut = 5),
+                    "WES FALS vs Normal" =
+                      mafCompare(maf_W_FALS, maf_NORMAL, "W_FALS", "Normal", minMut = 5),
+                    "Targeted vs WES M-SALS" =
+                      mafCompare(maf_T_M_SALS, maf_W_M_SALS, "Targeted", "WES", minMut = 5),
+                    "Targeted vs WES F-SALS" =
+                      mafCompare(maf_T_F_SALS, maf_W_F_SALS, "Targeted", "WES", minMut = 5),
+                    "Targeted vs WES FALS" =
+                      mafCompare(maf_T_FALS, maf_W_FALS, "Targeted", "WES", minMut = 5)
+      )
       
-    } else if (view == "Targeted FALS vs Normal") {
-      forestPlot(mafCompare(maf_T_FALS, maf_NORMAL,
-                            "Targeted FALS", "Normal"))
-      
-    } else if (view == "WES M-SALS vs Normal") {
-      forestPlot(mafCompare(maf_W_M_SALS, maf_NORMAL,
-                            "WES M-SALS", "Normal"))
-      
-    } else if (view == "WES F-SALS vs Normal") {
-      forestPlot(mafCompare(maf_W_F_SALS, maf_NORMAL,
-                            "WES F-SALS", "Normal"))
-      
-    } else if (view == "WES FALS vs Normal") {
-      forestPlot(mafCompare(maf_W_FALS, maf_NORMAL,
-                            "WES FALS", "Normal"))
-      
-    } else if (view == "Targeted M-SALS vs WES M-SALS") {
-      forestPlot(mafCompare(maf_T_M_SALS, maf_W_M_SALS,
-                            "Targeted M-SALS", "WES M-SALS"))
-      
-    } else if (view == "Targeted F-SALS vs WES F-SALS") {
-      forestPlot(mafCompare(maf_T_F_SALS, maf_W_F_SALS,
-                            "Targeted F-SALS", "WES F-SALS"))
-      
-    } else if (view == "Targeted FALS vs WES FALS") {
-      forestPlot(mafCompare(maf_T_FALS, maf_W_FALS,
-                            "Targeted FALS", "WES FALS"))
+      forestPlot(cmp, pVal = 0.05)
       
     } else if (view == "TMB") {
-      ggplot(tmb(maf), aes(x = "", y = total_perMB)) +
+      ggplot(tmb(maf), aes(y = total_perMB)) +
         geom_boxplot() +
         ylab("Mutations / Mb") +
         theme_minimal()
       
     } else if (view == "Ti/Tv") {
       plotTiTv(titv(maf))
+      
+    } else {
+      plot.new()
     }
   }
   
@@ -213,54 +203,32 @@ server <- function(input, output, session) {
   })
   
   output$summaryTable <- renderTable({
-    if (input$view == "Data Table") {
+    if (input$view == "Gene Mutation Frequency")
+      maf_active()@gene.summary[order(-MutatedSamples)]
+    else if (input$view == "Data Table")
       head(maf_active()@data, 50)
-    }
   })
   
-  # ===============================
-  # DOWNLOAD PLOT
-  # ===============================
-  
   output$download_plot <- downloadHandler(
-    filename = function() {
-      paste0(gsub(" ", "_", input$view), "_", Sys.Date(), ".png")
-    },
+    filename = function() paste0(gsub(" ", "_", input$view), ".png"),
     content = function(file) {
-      png(file, width = 2400, height = 1800, res = 300)
+      png(file, 2400, 1800, res = 300)
       make_plot(input$view, maf_active(), input$gene)
       dev.off()
     }
   )
   
-  # ===============================
-  # DOWNLOAD TABLE
-  # ===============================
-  
   output$download_table <- downloadHandler(
-    filename = function() {
-      paste0(gsub(" ", "_", input$view), "_", Sys.Date(), ".csv")
-    },
+    filename = function() paste0(gsub(" ", "_", input$view), ".csv"),
     content = function(file) {
-      
-      df <- switch(
-        input$view,
-        "Data Table" = maf_active()@data,
-        "Overview"   = getSampleSummary(maf_active()),
-        NULL
+      fwrite(
+        if (input$view == "Gene Mutation Frequency")
+          maf_active()@gene.summary
+        else maf_active()@data,
+        file
       )
-      
-      if (!is.null(df)) {
-        fwrite(df, file)
-      }
     }
   )
-  
 }
 
-# ===============================
-# RUN APP
-# ===============================
-
 shinyApp(ui, server)
-
